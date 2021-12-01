e001da30f05e71e428e07ab11d157867fd1af1be
Jesus <jesusalejandroap11@gmail.com>
2021-12-01 15:01:44 -0400

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+import { NbLayoutModule, NbThemeModule,NbButtonModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SquareComponent,
+    BoardComponent
+  ],
+  imports: [
+    BrowserModule,
+		AppRoutingModule,
+		NbThemeModule.forRoot({name: 'cosmic'}),
+		NbLayoutModule,
+		NbEvaIconsModule,
+		NbButtonModule,
+		BrowserAnimationsModule,
+		NbThemeModule.forRoot({ name: 'cosmic' })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
